b502b078e2fd19d92479dc87315635a9a0d96548
Keshav Poojary <richiehydra22@gmail.com>
2025-03-25 21:16:24 +0530

    add session layer

--- osi-frontend/src/app/components/Osi_Components/OsiVisualization.tsx
+++ osi-frontend/src/app/components/Osi_Components/OsiVisualization.tsx
@@ -6,6 +6,7 @@ import TlsHandshake from "../Presentation_Layer/TlsHandshake";
 import ApplicationBase from "../Application_Layer/Application_Base";
 import NetworkBase from "../Network_Layer/NetworkBase";
 import PhysicalLayer from "../Physical_Layer/Physical";
+import SessionLayer from "../Session_Layer/Session";
 
 interface NetworkLayerData {
   IP: string;
@@ -32,8 +33,8 @@ type OsiVisualizationProps = {
 const OsiVisualization: React.FC<OsiVisualizationProps> = ({ osiData, url, currentStep, isp }) => {
   const osiLayers = [
     { title: "Application Layer", content: <ApplicationBase domain={url} /> },
-    { title: "Presentation Layer", content: <TlsHandshake url={url}/> },
-    { title: "Session Layer", content: "TCP Handshake Verified" },
+    { title: "Presentation Layer", content: <TlsHandshake url={url} /> },
+    { title: "Session Layer", content: <SessionLayer /> },
     { title: "Transport Layer", content: <ThreeWayHandshake /> },
     {
       title: "Network Layer",
