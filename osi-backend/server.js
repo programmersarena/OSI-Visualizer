5f4723b59daaa1881ff3c14c901fbd1ef7e3da15
Keshav Poojary <richiehydra22@gmail.com>
2025-05-02 00:12:44 +0530

    add socket url

--- osi-backend/server.js
+++ osi-backend/server.js
@@ -6,7 +6,7 @@ import routes from './src/routes.js';
 
 const app = express();
 
-const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+const FRONTEND_URL = process.env.NEXT_SOCKET_FRONTEND_URL  || 'http://localhost:3000';
 
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
